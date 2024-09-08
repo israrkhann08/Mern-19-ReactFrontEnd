be387dfd1a388dca47f5da41a87b8fa7a7d700e4
israrkhann08 <israrkhann08@gmail.com>
2024-09-08 11:46:51 +0500

    Input field to add items

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 import Counter from "./components/Counter";
 
 function App() {
-  const iteams = ["Jeans", "Jackets", "Shirts"];
+  const [items, setItems] = useState(["Jeans", "Jackets"]);
+  const [userInput, setUserTnput] = useState("");
+
+  const inputChangeHandler = (e) => {
+    setUserTnput(e.target.value);
+      //  console.log(e.target.value);
+  };
+
+  const addItemInputHandler = () => {
+    setItems([...items, userInput]);
+  };
 
   return (
     <>
-      {iteams.map((item, index, arr) => {
+    <input onChange={inputChangeHandler}  placeholder="Enter iTeam Name"  style={{'padding': '7px'}} />
+    <button onClick={addItemInputHandler}>Add Item</button>
+      {items.map((item, index, arr) => {
         return <Counter itemName={item} />;
       })}
     </>
